aa434dea0d19c9197abd408582bec0f5f5919ade
Kevin Li <kevinli@Kevins-MacBook-Pro.local>
2019-08-11 19:53:58 -0400

    tabView times

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Plot from 'react-plotly.js';
 import moment from 'moment';
+import { Tabs, Tab } from 'react-mdl';
 
 
 class Dashboard extends Component{
@@ -17,22 +18,43 @@ class Dashboard extends Component{
             layout: {
                 datarevision: 0,
             },
-            timeView: 'http://127.0.0.1:5000'
+            timeViewURL: 'http://127.0.0.1:5000',
+            timeView: 'All',
+            activeTab: 4
         }
-        this.REFRESH_TIME = 2000;
 
-        this.handleToggleTimeViewDay = this.handleToggleTimeViewDay.bind(this);
-        this.handleToggleTimeViewWeek = this.handleToggleTimeViewWeek.bind(this);
-        this.handleToggleTimeViewMonth = this.handleToggleTimeViewMonth.bind(this);
-        this.handleToggleTimeViewYear = this.handleToggleTimeViewYear.bind(this);
-        this.handleToggleTimeViewAll = this.handleToggleTimeViewAll.bind(this);
-
+        this.REFRESH_TIME = 3000;
+        this.handleToggleTimeView =this.handleToggleTimeView.bind(this);
     }
 
-    handleToggleTimeViewDay() {
-        let start = moment().format('YYYY-MM-DD');
-        let timeQuery = 'http://127.0.0.1:5000?start_date=' + start;
-        console.log('start', start);
+    handleToggleTimeView(tabId) {
+        console.log(moment().format('LLLL'));
+        this.setState({ activeTab: tabId});
+        let end = moment().format('YYYY-MM-DD');
+        let start = '';
+        let timeQuery = '';
+        let timeView = '';
+
+        if (tabId < 4) {
+            if (tabId === 0) {
+                timeView = 'Day'
+                start = moment().format('YYYY-MM-DD');
+            } else if (tabId === 1) {
+                timeView = 'Week'
+                start = moment().subtract(7, 'days').format('YYYY-MM-DD');
+            } else if (tabId === 2) {
+                timeView = 'Month'
+                start = moment().subtract(1, 'months').format('YYYY-MM-DD');
+            } else {
+                timeView = 'Year'
+                start = moment().subtract(1, 'years').format('YYYY-MM-DD');
+            } 
+            timeQuery = 'http://127.0.0.1:5000?start_date=' + start + '&end_date=' + end;
+        } else {
+            timeView = 'All'
+            timeQuery = 'http://127.0.0.1:5000?end_date=' + end;
+        }
+        console.log('start', start, 'end', end);
         this.setState({
             time: [],
             hold: [],
@@ -41,85 +63,13 @@ class Dashboard extends Component{
             layout: {
                 datarevision: 0,
             },
-            timeView : timeQuery
+            timeViewURL : timeQuery,
+            timeView
         });
         this.state.layout.datarevision = this.state.revision + 1;
+
     }
 
-    handleToggleTimeViewWeek() {
-        let end = moment().format('YYYY-MM-DD');
-        let start = moment().subtract(7, 'days').format('YYYY-MM-DD');
-        let timeQuery = 'http://127.0.0.1:5000?start_date=' + start + '&end_date=' + end;
-        console.log('start', start, 'end', end);
-        this.setState(() => {
-            return {
-                time: [],
-                hold: [],
-                latency: [],
-                revision: 0,
-                layout: {
-                    datarevision: 0,
-                },
-                timeView : timeQuery
-            };
-        })
-    }
-    handleToggleTimeViewMonth() {
-        let end = moment().format('YYYY-MM-DD');
-        let start = moment().subtract(1, 'months').format('YYYY-MM-DD');
-        let timeQuery = 'http://127.0.0.1:5000?start_date=' + start + '&end_date=' + end;
-        console.log('start', start, 'end', end);
-        this.setState(() => {
-            return {
-                time: [],
-                hold: [],
-                latency: [],
-                revision: 0,
-                layout: {
-                    datarevision: 0,
-                },
-                timeView : timeQuery
-            };
-        })
-    }
-    handleToggleTimeViewYear() {
-        let end = moment().format('YYYY-MM-DD');
-        let start = moment().subtract(1, 'years').format('YYYY-MM-DD');
-        let timeQuery = 'http://127.0.0.1:5000?start_date=' + start + '&end_date=' + end;
-        console.log('start', start, 'end', end);
-        this.setState(() => {
-            return {
-                time: [],
-                hold: [],
-                latency: [],
-                revision: 0,
-                layout: {
-                    datarevision: 0,
-                },
-                timeView : timeQuery
-            };
-        })
-    }
-    handleToggleTimeViewAll() {
-        let end = moment().format('YYYY-MM-DD');
-        let timeQuery = 'http://127.0.0.1:5000?end_date=' + end;
-        console.log('end', end);
-        this.setState(() => {
-            return {
-                time: [],
-                hold: [],
-                latency: [],
-                revision: 0,
-                layout: {
-                    datarevision: 0,
-                },
-                timeView : timeQuery
-            };
-        })
-    }
-
-
-    
     async componentDidMount() {
 
         try {
@@ -128,7 +78,7 @@ class Dashboard extends Component{
                 console.log("get")
 
                 // const response = await fetch(this.urlData,
-                const response = await fetch(this.state.timeView,
+                const response = await fetch(this.state.timeViewURL,
                     {
                         headers : { 
                             'Content-Type': 'application/json',
@@ -165,17 +115,23 @@ class Dashboard extends Component{
         return (
             <div className="Dashboard">
                 <h1> Hello, Jackie.</h1>
-                <h4> <b>Today is August 4, 2019.</b></h4>
+                <h4> <b>Today is {moment().format('LL')}.</b></h4>
                 <br></br><br></br>
                 <p>See your recent activity.</p>
+                <br></br>
 
-                <div className="toggleTime">
-                    <button className="toggleTimeButton" id="DayView" value="Day" onClick = {this.handleToggleTimeViewDay}>Day</button>
-                    <button className="toggleTimeButton" id="WeekView" value="Week" onClick = {this.handleToggleTimeViewWeek}>Week</button>
-                    <button className="toggleTimeButton" id="MonthyView" value="Month" onClick = {this.handleToggleTimeViewMonth}>Month</button>
-                    <button className="toggleTimeButton" id="YearView" value="Year" onClick = {this.handleToggleTimeViewYear}>Year</button>
-                    <button className="toggleTimeButton" id="AllView" value="ALl" onClick = {this.handleToggleTimeViewAll}>All</button>
-                </div>
+                <div className="demo-tabs">
+                    <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.handleToggleTimeView(tabId)} ripple>
+                        <Tab>Day</Tab>
+                        <Tab>Week</Tab>
+                        <Tab>Month</Tab>
+                        <Tab>Year</Tab>
+                        <Tab>All</Tab>
+                    </Tabs>
+                    <section>
+                        <div className="content"><h4>Viewing: {this.state.timeView}</h4></div>
+                    </section>
+                </div> 
 
                 <Plot
                 data={[
@@ -213,3 +169,114 @@ class Dashboard extends Component{
 }
 
 export default Dashboard;
+
+
+
+
+// this.handleToggleTimeViewDay = this.handleToggleTimeViewDay.bind(this);
+// this.handleToggleTimeViewWeek = this.handleToggleTimeViewWeek.bind(this);
+// this.handleToggleTimeViewMonth = this.handleToggleTimeViewMonth.bind(this);
+// this.handleToggleTimeViewYear = this.handleToggleTimeViewYear.bind(this);
+// this.handleToggleTimeViewAll = this.handleToggleTimeViewAll.bind(this);
+
+// <div className="toggleTime">
+// <button className="toggleTimeButton" id="DayView" value="Day" onClick = {this.handleToggleTimeViewDay}>Day</button>
+// <button className="toggleTimeButton" id="WeekView" value="Week" onClick = {this.handleToggleTimeViewWeek}>Week</button>
+// <button className="toggleTimeButton" id="MonthyView" value="Month" onClick = {this.handleToggleTimeViewMonth}>Month</button>
+// <button className="toggleTimeButton" id="YearView" value="Year" onClick = {this.handleToggleTimeViewYear}>Year</button>
+// <button className="toggleTimeButton" id="AllView" value="All" onClick = {this.handleToggleTimeViewAll}>All</button>
+// </div>
+
+// handleToggleTimeViewDay() {
+//     let start = moment().format('YYYY-MM-DD');
+//     let timeQuery = 'http://127.0.0.1:5000?start_date=' + start;
+//     console.log('start', start);
+//     this.setState({
+//         time: [],
+//         hold: [],
+//         latency: [],
+//         revision: 0,
+//         layout: {
+//             datarevision: 0,
+//         },
+//         timeViewURL : timeQuery,
+//         timeView: 'day'
+//     });
+//     this.state.layout.datarevision = this.state.revision + 1;
+// }
+
+// handleToggleTimeViewWeek() {
+//     let end = moment().format('YYYY-MM-DD');
+//     let start = moment().subtract(7, 'days').format('YYYY-MM-DD');
+//     let timeQuery = 'http://127.0.0.1:5000?start_date=' + start + '&end_date=' + end;
+//     console.log('start', start, 'end', end);
+//     this.setState(() => {
+//         return {
+//             time: [],
+//             hold: [],
+//             latency: [],
+//             revision: 0,
+//             layout: {
+//                 datarevision: 0,
+//             },
+//             timeViewURL : timeQuery,
+//             timeView: 'week'
+//         };
+//     })
+// }
+// handleToggleTimeViewMonth() {
+//     let end = moment().format('YYYY-MM-DD');
+//     let start = moment().subtract(1, 'months').format('YYYY-MM-DD');
+//     let timeQuery = 'http://127.0.0.1:5000?start_date=' + start + '&end_date=' + end;
+//     console.log('start', start, 'end', end);
+//     this.setState(() => {
+//         return {
+//             time: [],
+//             hold: [],
+//             latency: [],
+//             revision: 0,
+//             layout: {
+//                 datarevision: 0,
+//             },
+//             timeViewURL : timeQuery,
+//             timeView: 'month'
+//         };
+//     })
+// }
+// handleToggleTimeViewYear() {
+//     let end = moment().format('YYYY-MM-DD');
+//     let start = moment().subtract(1, 'years').format('YYYY-MM-DD');
+//     let timeQuery = 'http://127.0.0.1:5000?start_date=' + start + '&end_date=' + end;
+//     console.log('start', start, 'end', end);
+//     this.setState(() => {
+//         return {
+//             time: [],
+//             hold: [],
+//             latency: [],
+//             revision: 0,
+//             layout: {
+//                 datarevision: 0,
+//             },
+//             timeViewURL : timeQuery,
+//             timeView: 'year'
+//         };
+//     })
+// }
+// handleToggleTimeViewAll() {
+//     let end = moment().format('YYYY-MM-DD');
+//     let timeQuery = 'http://127.0.0.1:5000?end_date=' + end;
+//     console.log('end', end);
+//     this.setState(() => {
+//         return {
+//             time: [],
+//             hold: [],
+//             latency: [],
+//             revision: 0,
+//             layout: {
+//                 datarevision: 0,
+//             },
+//             timeViewURL : timeQuery,
+//             timeView: 'all'
+//         };
+//     })
+// }
